05b4cc96ccc34907a2f5b5e4cd7b323d1be9a715
didisaprudin2104 <didisaprudin2104@gmail.com>
2025-01-19 13:16:29 +0700

    commit_buuton_warna

--- resources/views/layouts/menu.blade.php
+++ resources/views/layouts/menu.blade.php
@@ -27,7 +27,7 @@
             </ul>
 
             <a href="#"
-                class="burger ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light"
+                class="burger-button ml-auto float-right site-menu-toggle js-menu-toggle d-inline-block d-lg-none light"
                 data-toggle="collapse" data-target="#main-navbar">
                 <button class="burger-button">
                     <i class="fas fa-bars"></i> <!-- Font Awesome bars icon -->
