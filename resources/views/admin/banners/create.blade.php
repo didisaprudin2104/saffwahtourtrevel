39ca382ca6453ca5a7ee6156b91979e045c0359b
didisaprudin2104 <didisaprudin2104@gmail.com>
2025-01-25 00:46:30 +0700

    commit_5

--- resources/views/admin/banners/create.blade.php
+++ resources/views/admin/banners/create.blade.php
@@ -2,7 +2,8 @@
     <div class="app-title">
         <div>
             <h1><i class="bi bi-airplane-engines"></i> Tabel Banner - Saffwah Tour Travel</h1>
-            <p>Discover the world with Saffwah Tour Travel. Affordable trips and personalized services for unforgettable journeys.</p>
+            <p>Discover the world with Saffwah Tour Travel. Affordable trips and personalized services for unforgettable
+                journeys.</p>
         </div>
         <ul class="app-breadcrumb breadcrumb">
             <li class="breadcrumb-item"><i class="bi bi-house-door fs-6"></i></li>
@@ -31,7 +32,8 @@
                         </div>
                         <div class="mb-3">
                             <label class="form-check-label" for="is_active">Active</label>
-        <input class="form-check-input" type="checkbox" name="is_active" id="is_active" value="1">
+                            <input class="form-check-input" type="checkbox" name="is_active" id="is_active"
+                                value="1">
                         </div>
                         <button class="btn btn-primary" type="submit">Submit</button>
                     </form>
@@ -39,4 +41,27 @@
             </div>
         </div>
     </div>
+    <script src="https://cdn.tiny.cloud/1/gdwactny6stknjckzytt0r1sn757exssk0p8wrx3gr2hn2n3/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
+
+<!-- Place the following <script> and <textarea> tags your HTML's <body> -->
+<script>
+  tinymce.init({
+    selector: 'textarea',
+    plugins: [
+      // Core editing features
+      'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+      // Your account includes a free trial of TinyMCE premium features
+      // Try the most popular premium features until Feb 6, 2025:
+      'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+    ],
+    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+    tinycomments_mode: 'embedded',
+    tinycomments_author: 'Author name',
+    mergetags_list: [
+      { value: 'First.Name', title: 'First Name' },
+      { value: 'Email', title: 'Email' },
+    ],
+    ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
+  });
+</script>
 </x-app-layout>
--- resources/views/admin/banners/edit.blade.php
+++ resources/views/admin/banners/edit.blade.php
@@ -57,12 +57,23 @@
     </div>
     <script src="https://cdn.tiny.cloud/1/gdwactny6stknjckzytt0r1sn757exssk0p8wrx3gr2hn2n3/tinymce/7/tinymce.min.js" referrerpolicy="origin"></script>
     <script>
-        // Initialize TinyMCE for the textarea with ID 'description'
         tinymce.init({
-            selector: '#description',  // The ID of your textarea
-            plugins: 'advlist autolink lists link image charmap print preview hr anchor pagebreak',
-            toolbar: 'undo redo | bold italic | alignleft aligncenter alignright | code',
-            height: 400  // Optional: Adjust the height of the editor
+          selector: 'textarea',
+          plugins: [
+            // Core editing features
+            'anchor', 'autolink', 'charmap', 'codesample', 'emoticons', 'image', 'link', 'lists', 'media', 'searchreplace', 'table', 'visualblocks', 'wordcount',
+            // Your account includes a free trial of TinyMCE premium features
+            // Try the most popular premium features until Feb 6, 2025:
+            'checklist', 'mediaembed', 'casechange', 'export', 'formatpainter', 'pageembed', 'a11ychecker', 'tinymcespellchecker', 'permanentpen', 'powerpaste', 'advtable', 'advcode', 'editimage', 'advtemplate', 'ai', 'mentions', 'tinycomments', 'tableofcontents', 'footnotes', 'mergetags', 'autocorrect', 'typography', 'inlinecss', 'markdown','importword', 'exportword', 'exportpdf'
+          ],
+          toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline strikethrough | link image media table mergetags | addcomment showcomments | spellcheckdialog a11ycheck typography | align lineheight | checklist numlist bullist indent outdent | emoticons charmap | removeformat',
+          tinycomments_mode: 'embedded',
+          tinycomments_author: 'Author name',
+          mergetags_list: [
+            { value: 'First.Name', title: 'First Name' },
+            { value: 'Email', title: 'Email' },
+          ],
+          ai_request: (request, respondWith) => respondWith.string(() => Promise.reject('See docs to implement AI Assistant')),
         });
-    </script>
+      </script>
 </x-app-layout>
